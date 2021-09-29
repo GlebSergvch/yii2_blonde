d667c9f9d573dd8fcc50dce98f284610dbe04d8d
Gleb Sergvch <glsergvch@gmail.com>
2021-09-29 23:35:22 +0500

    BlogController для вывода нескольких и одной записи. Настройка urlManager во фронтенд. Пофиксил лайаут.

--- yii/frontend/views/layouts/main.php
+++ yii/frontend/views/layouts/main.php
@@ -36,8 +36,9 @@ AppAsset::register($this);
     ]);
     $menuItems = [
         ['label' => 'Home', 'url' => ['/site/index']],
-        ['label' => 'About', 'url' => ['/site/about']],
-        ['label' => 'Contact', 'url' => ['/site/contact']],
+        ['label' => 'Блог', 'url' => ['/blog/index']],
+        ['label' => 'О нас', 'url' => ['/site/about']],
+        ['label' => 'Контакты', 'url' => ['/site/contact']],
     ];
     if (Yii::$app->user->isGuest) {
         $menuItems[] = ['label' => 'Signup', 'url' => ['/site/signup']];
